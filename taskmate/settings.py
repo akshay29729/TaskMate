8483207987c949140f246bf7fece0a701dc89da2
akshay20729 <akshayshinde29729@gmail.com>
2021-06-19 14:21:34 +0530

    heroku library added

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import environ
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(SECRET_KEY=str,)
@@ -134,3 +135,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = "login"
+django_heroku.settings(locals())
